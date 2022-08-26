829dd4dede9e9900206029ceadbd9f0f62cfef41
Colm Faherty <colm.faherty@blueface.com>
2022-08-26 18:07:52 +0100

    Initial Commit

---
+++ io_asyncio.py
@@ -0,0 +1,24 @@
+import asyncio
+import time
+import aiohttp
+
+
+async def download_site(session, url):
+    async with session.get(url) as response:
+        print(f'Read {response.content_lenth} from {url}')
+
+
+async def download_all_sites(sites):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in sites:
+            task = asyncio.ensure_future(download_site(session, url))
+            tasks.append(task)
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+if __name__ == '__main__':
+    sites = ['https://www.jython.org', 'http://olympus.realpython.org/dice'] * 80
+    start_time = time.time()
+    asyncio.run(download_all_sites(sites))
+    duration = time.time() - start_time
+    print(f'Downloaded {len(sites)} in {duration} seconds')
